783433cfe57ed9536348f8266fdddb79313c2d70
peter-j <peter.j@kakaocorp.com>
2023-06-16 23:04:19 +0900

    Initial Commit

---
+++ StudySwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  StudySwiftUI
+//
+//  Created by peter on 2023/06/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StudySwiftUI/StudySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudySwiftUIApp.swift
+//  StudySwiftUI
+//
+//  Created by peter on 2023/06/16.
+//
+
+import SwiftUI
+
+@main
+struct StudySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
